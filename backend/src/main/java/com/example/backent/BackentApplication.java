9e69a8c421eb2815daafc698604a46d87f5ef451
qudratjon <qudratjon03031999>
2021-06-02 18:32:26 +0500

    first commit

---
+++ backend/src/main/java/com/example/backent/BackentApplication.java
@@ -0,0 +1,13 @@
+package com.example.backent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackentApplication.class, args);
+    }
+
+}
